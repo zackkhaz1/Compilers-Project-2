2f0b7a04c9a1eeb7ca6d3687a38eed00628fdf51
zackkhaz1 <54860278+zackkhaz1@users.noreply.github.com>
2021-02-18 17:39:25 -0600

    Starter files

---
+++ main.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include "errors.hpp"
+#include "scanner.hpp"
+
+using namespace crona;
+
+static void usageAndDie(){
+	std::cerr << "Usage: cronac <infile>"
+	<< " [-p]: Parse the input to check syntax\n"
+	<< " [-t <tokensFile>]: Output tokens to <tokensFile>\n"
+	;
+	exit(1);
+}
+
+static void writeTokenStream(const char * inPath, const char * outPath){
+	std::ifstream inStream(inPath);
+	if (!inStream.good()){
+		std::string msg = "Bad input stream";
+		msg += inPath;
+		throw new InternalError(msg.c_str());
+	}
+	if (outPath == nullptr){
+		std::string msg = "No tokens output file given";
+		throw new InternalError(msg.c_str());
+	}
+
+	Scanner scanner(&inStream);
+	if (strcmp(outPath, "--") == 0){
+		scanner.outputTokens(std::cout);
+	} else {
+		std::ofstream outStream(outPath);
+		if (!outStream.good()){
+			std::string msg = "Bad output file ";
+			msg += outPath;
+			throw new InternalError(msg.c_str());
+		}
+		scanner.outputTokens(outStream);
+		outStream.close();
+	}
+}
+
+static bool parse(const char * inFile){
+	std::ifstream inStream(inFile);
+	if (!inStream.good()){
+		std::string msg = "Bad input stream ";
+		msg += inFile;
+		throw new InternalError(msg.c_str());
+	}
+
+	crona::Scanner scanner(&inStream);
+	crona::Parser parser(scanner);
+	int errCode = parser.parse();
+	if (errCode != 0){ return false; }
+
+	return true;
+}
+
+int 
+main( const int argc, const char **argv )
+{
+	if (argc == 0){
+		usageAndDie();
+	}
+	const char * inFile = NULL;
+	const char * tokensFile = NULL;
+	bool checkParse = false;
+	bool useful = false;
+	int i = 1;
+	for (int i = 1 ; i < argc ; i++){
+		if (argv[i][0] == '-'){
+			if (argv[i][1] == 't'){
+				i++;
+				tokensFile = argv[i];
+				useful = true;
+			} else if (argv[i][1] == 'p'){
+				i++;
+				checkParse = true;
+				useful = true;
+			} else {
+				std::cerr << "Unrecognized argument: ";
+				std::cerr << argv[i] << std::endl;
+				usageAndDie();
+			}
+		} else {
+			if (inFile == NULL){
+				inFile = argv[i];
+			} else {
+				std::cerr << "Only 1 input file allowed";
+				std::cerr << argv[i] << std::endl;
+				usageAndDie();
+			}
+		}
+	}
+	if (inFile == NULL){
+		usageAndDie();
+	}
+	if (!useful){
+		std::cerr << "Hey, you didn't tell cronac to do anything!\n";
+		usageAndDie();
+	}
+
+	if (tokensFile != NULL){
+		try {
+			writeTokenStream(inFile, tokensFile);
+		} catch (InternalError * e){
+			std::cerr << "Error: " << e->msg() << std::endl;
+		}
+	}
+
+	if (checkParse){
+		try {
+			bool parsed = parse(inFile);
+			if (!parsed){
+				std::cerr << "Parse failed" << std::endl;
+			}
+		} catch (ToDoError * e){
+			std::cerr << "ToDo: " << e->msg() << std::endl;
+			exit(1);
+		}
+	}
+	
+	return 0;
+}
